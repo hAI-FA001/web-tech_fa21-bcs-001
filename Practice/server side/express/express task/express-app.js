fc242944f981f7e7e4e3c9327b4eaad51dd6f992
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-03-20 20:59:16 +0500

    created express app/server for landing page, cv, contact us, api views

---
+++ Practice/server side/express/express task/express-app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+let server = express();
+
+server.set("view engine", "ejs");
+server.use(express.static("public"));
+
+server.get("/", function (req, res) {
+  res.render("landing-page");
+});
+
+server.get("/cv", function (req, res) {
+  res.render("cv-page");
+});
+
+server.get("/contact", function (req, res) {
+  res.render("contact-page");
+});
+
+server.get("/api/restful-api", function (req, res) {
+  res.render("api-page");
+});
+
+server.listen(4000);
