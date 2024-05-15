39f725ae967d43897131f0ffba05ba8aa9a8822b
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-05-15 20:29:24 +0500

    redone filters logic (used what mongoose provides), fixed pagination (shows correct number of pages + pagination applied on filtered products + fixed next button's pagination logic)

--- Practice/server side/express/project mvc task/routes/products.js
+++ Practice/server side/express/project mvc task/routes/products.js
@@ -15,37 +15,39 @@ router.get("/checkout", checkSessionAuth, async (req, res) => {
 });
 
 router.get("/:pageNumber?", async (req, res) => {
-  let pageNumber = req.params.pageNumber ? req.params.pageNumber : 1;
-  pageNumber = parseInt(pageNumber);
+  let filters = {};
 
-  let totalPages = await Product.countDocuments();
-  totalPages = parseInt(totalPages);
-
-  let numberToShow = 10;
-  let skip = (pageNumber - 1) * numberToShow;
-
-  let products = await Product.find().limit(numberToShow).skip(skip);
+  if (req.session.product) {
+    // i = case insensitive
+    filters.name = { $regex: req.session.product, $options: "i" };
+  }
 
   if (req.session.filters) {
-    let productName = req.session.product;
-
     let startingPrice = req.session.filters.startingPrice;
     let endingPrice = req.session.filters.endingPrice;
     let onSale = req.session.filters.onSale;
     let rating = req.session.filters.productRating;
 
-    if (productName) {
-      products = products.filter((p) =>
-        p.name.toLowerCase().includes(productName.toLowerCase())
-      );
-    }
-
     if (startingPrice && endingPrice) {
-      products = products.filter(
-        (p) => p.price >= startingPrice && p.price <= endingPrice
-      );
+      filters.price = { $gte: startingPrice, $lte: endingPrice };
     }
   }
+
+  let pageNumber = req.params.pageNumber ? req.params.pageNumber : 1;
+  pageNumber = parseInt(pageNumber);
+
+  let numProductsToShow = 10;
+  let numProductsToSkip = (pageNumber - 1) * numProductsToShow;
+
+  let totalPages = await Product.find(filters).count();
+  totalPages = Math.ceil(parseInt(totalPages) / numProductsToShow);
+
+  // a/c to docs, lean = faster, but cannot do these: change tracking, casting & validation, getter/setter, virtuals, save()
+  products = await Product.find(filters)
+    .sort({ name: 1 })
+    .limit(numProductsToShow)
+    .skip(numProductsToSkip)
+    .lean();
 
   let cart = req.session.cart ? req.session.cart : [];
 
@@ -58,6 +60,7 @@ router.get("/:pageNumber?", async (req, res) => {
 });
 
 router.post("/:pageNumber?", async (req, res) => {
+  // at a time, only 1 form is submitted (product name or filters)
   if (typeof req.body.product == "string") {
     req.session.product = req.body.product;
   } else if (typeof req.body != typeof undefined) {
