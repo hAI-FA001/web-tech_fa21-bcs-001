f7b5d294991671a4b2a966304685296c59e347e0
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-05-17 21:49:44 +0500

    added more properties & virtuals in Product, used req.body in API instead of listing each property, used virtual properties in ejs (trending, top rated, discount calc, etc)

--- Practice/server side/express/project mvc task/models/Product.js
+++ Practice/server side/express/project mvc task/models/Product.js
@@ -1,10 +1,37 @@
 const mongoose = require("mongoose");
 
-let productSchema = mongoose.Schema({
-  name: String,
-  price: Number,
-  stock: Number,
-  category: String,
+let productSchema = mongoose.Schema(
+  {
+    name: String,
+    price: Number,
+    stock: Number,
+    category: String,
+
+    rating: Number,
+    totalRatings: Number,
+    imageUrl: String,
+    discount: Number,
+    dateAdded: Date,
+  },
+  // note/reminder: add this otherwise mongoose will not add virtuals when it converts document to JSON
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+// note/reminder: don't use arrow functions because "this" keyword won't work
+productSchema.virtual("isOutOfStock").get(function () {
+  return this.stock == 0;
+});
+productSchema.virtual("isTopRated").get(function () {
+  return this.rating >= 4.5 && this.totalRatings > 10;
+});
+productSchema.virtual("isOnSale").get(function () {
+  return this.discount > 0;
+});
+productSchema.virtual("isTrending").get(function () {
+  let today = new Date();
+  let prevWeek = new Date();
+  prevWeek.setDate(today.getDate() - 7);
+  return this.isTopRated && today - this.dateAdded <= today - prevWeek;
 });
 
 let productModel = mongoose.model("Product", productSchema);
