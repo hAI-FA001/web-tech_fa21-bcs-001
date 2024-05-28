ff98be1f8d533166c480f24d88c682d23f2d998a
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-05-28 20:05:11 +0500

    separated project into Final folder for final lab exam + check if it still works (it works), some react files taught in prev class

---
+++ Final/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+let userSchema = mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  verificationCode: String,
+  isActivated: Boolean,
+
+  myPurchases: [
+    mongoose.Schema({
+      _id: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+      name: String,
+      price: Number,
+      category: String,
+      imageUrl: String,
+    }),
+  ],
+
+  roles: [String],
+});
+
+let userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
